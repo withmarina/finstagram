129c3f4fcb2c1879e6e967e6045dd2e5ee0f370a
Marina Ghosh <kosyachenkom@gmail.com>
2021-07-17 22:37:03 +0000

    W3D1

--- app/actions.rb
+++ app/actions.rb
@@ -13,7 +13,7 @@ def humanized_time_ago(time_ago_in_minutes)
   end
   
   get '/' do
-    finstagram_post_shark = {
+    @finstagram_post_shark = {
       username: "sharky_j",
       avatar_url: "http://naserca.com/images/sharky_j.jpg",
       photo_url: "http://naserca.com/images/shark.jpg",
@@ -26,7 +26,7 @@ def humanized_time_ago(time_ago_in_minutes)
       }]
     }
   
-    finstagram_post_whale = {
+    @finstagram_post_whale = {
         username: "kirk_whalum",
         avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
         photo_url: "http://naserca.com/images/whale.jpg",
@@ -39,7 +39,7 @@ def humanized_time_ago(time_ago_in_minutes)
         }]
       }
     
-      finstagram_post_marlin = {
+      @finstagram_post_marlin = {
         username: "marlin_peppa",
         avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
         photo_url: "http://naserca.com/images/marlin.jpg",
@@ -52,5 +52,7 @@ def humanized_time_ago(time_ago_in_minutes)
         }]
       }
 
-    [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+    # [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
+    @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+    erb(:index)
   end
