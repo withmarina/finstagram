b1dc7556a8489d2c4063c8b57d86dd65288ce6d6
Marina Ghosh <kosyachenkom@gmail.com>
2021-07-24 21:58:18 +0000

    W4D1

--- app/actions.rb
+++ app/actions.rb
@@ -2,57 +2,7 @@
 #     File.read(File.join('app/views', 'index.html'))
 # end
 
-def humanized_time_ago(time_ago_in_minutes)
-    if time_ago_in_minutes >= 120
-      "#{time_ago_in_minutes / 60} hours ago"
-    elsif time_ago_in_minutes >= 60
-      "#{time_ago_in_minutes / 60} hour ago"
-    else
-      "#{time_ago_in_minutes} minutes ago"
-    end
-  end
-  
-  get '/' do
-    @finstagram_post_shark = {
-      username: "sharky_j",
-      avatar_url: "http://naserca.com/images/sharky_j.jpg",
-      photo_url: "http://naserca.com/images/shark.jpg",
-      humanized_time_ago: humanized_time_ago(15),
-      like_count: 0,
-      comment_count: 1,
-      comments: [{
-          username: "sharky_j",
-          text: "Out for the long weekend... rocking the beach bod y'all!"
-      }]
-    }
-  
-    @finstagram_post_whale = {
-        username: "kirk_whalum",
-        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-        photo_url: "http://naserca.com/images/whale.jpg",
-        humanized_time_ago: humanized_time_ago(65),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-          username: "kirk_whalum",
-          text: "#weekendvibes"
-        }]
-      }
-    
-      @finstagram_post_marlin = {
-        username: "marlin_peppa",
-        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-        photo_url: "http://naserca.com/images/marlin.jpg",
-        humanized_time_ago: humanized_time_ago(190),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-          username: "marlin_peppa",
-          text: "lunchtime! ;)"
-        }]
-      }
-
-    # [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
-    @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
-    erb(:index)
-  end
+get '/' do
+  @finstagram_posts = FinstagramPost.order(created_at: :desc)
+  erb(:index)
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
 
+  has_many :comments
+  has_many :likes
+  has_many :finstagram_posts
+
 end
