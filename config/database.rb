96a5db33c4232f6b7f888ea26808f5a0a97d6f22
Marina Ghosh <kosyachenkom@gmail.com>
2021-08-08 16:37:14 +0000

    W6D2 Updated database config

--- config/database.rb
+++ config/database.rb
@@ -1,24 +1,3 @@
-# configure do
-#   # Log queries to STDOUT in development
-#   if Sinatra::Application.development?
-#     ActiveRecord::Base.logger = Logger.new(STDOUT)
-#   end
-
-#   set :database, {
-#     adapter: "sqlite3",
-#     database: "db/db.sqlite3"
-#   }
-
-#   # Load all models from app/models, using autoload instead of require
-#   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
-#   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
-#     filename = File.basename(model_file).gsub('.rb', '')
-#     autoload ActiveSupport::Inflector.camelize(filename), model_file
-#   end
-
-# end
-
-# deployment database file:
 configure do
   # Log queries to STDOUT in development
   if Sinatra::Application.development?
@@ -31,7 +10,7 @@ configure do
       database: "db/db.sqlite3"
     }
   else
-    db_url = 'postgres://[your_postgres_url here]'
+    db_url = 'postgres://htxymehocomvsh:1cd08db9e29b6e63091239219abd11c188ec78e844a652a40d97d90aee419cee@ec2-3-218-149-60.compute-1.amazonaws.com:5432/d8mq0265bpmk29'
     db = URI.parse(ENV['DATABASE_URL'] || db_url)
     set :database, {
       adapter: "postgresql",
